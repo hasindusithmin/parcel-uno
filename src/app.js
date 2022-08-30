54ff2c0c1024ccfb5356293a30d4ce98aa26c93d
hasindu sithmin <hasindusithmin64@gmail.com>
2022-08-30 06:41:24 +0000

    1st test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+
+if (!(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i))) {
+    document.body.style.cssText = "width:60%;margin:auto;"
+}
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
+    document.getElementById('desc').className = 'w3-half w3-padding'
+}
+
+document.getElementById('alert').onclick = e=>{
+    const txt = `${process.env.UNAME}:${process.env.PWORD}`
+    fetch('https://4i7ncv.deta.dev/',{
+        method:"GET",
+        headers: {
+            'Authorization':`Basic ${btoa(txt)}`
+        }
+    })
+        .then(res=>{
+            alert(res.statusText)
+        })
+}
